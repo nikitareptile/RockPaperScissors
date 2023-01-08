979dc02b574d9eac4c9a01ce92a86981cc026a64
Nikita Tikhonyuk <nikitatikhonyuk@yandex.com>
2023-01-08 18:34:12 +1000

    Created main game screen

--- RockPaperScissors/ContentView.swift
+++ RockPaperScissors/ContentView.swift
@@ -8,12 +8,59 @@
 import SwiftUI
 
 struct ContentView: View {
+    var score = 0
+    var highscore = 0
+    
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+            HStack {
+                Text("✊")
+                    .font(.largeTitle)
+                Text("✋")
+                    .font(.largeTitle)
+                Text("✌️")
+                    .font(.largeTitle)
+            }
+            Text("The game")
+                .font(.title)
+                .fontWeight(.semibold)
+            Spacer()
+            HStack {
+                Spacer()
+                VStack {
+                    Text("✊")
+                        .font(.system(size: 100))
+                    Text("You")
+                }
+                Spacer()
+                VStack {
+                    Text("✌️")
+                        .font(.system(size: 100))
+                    Text("Opponent")
+                }
+                Spacer()
+            }
+            Spacer()
+            VStack {
+                HStack {
+                    Spacer()
+                    Text("✊")
+                        .font(.system(size: 80))
+                    Spacer()
+                    Text("✋")
+                        .font(.system(size: 80))
+                    Spacer()
+                    Text("✌️")
+                        .font(.system(size: 80))
+                    Spacer()
+                }
+                Text("Choose next")
+            }
+            Spacer()
+            VStack {
+                Text("Current score is \(score)")
+                Text("Your highscore is \(highscore)")
+            }
         }
         .padding()
     }
