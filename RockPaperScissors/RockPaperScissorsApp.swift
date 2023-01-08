feafc59b1958ffb92ae3dd2aa503a61183181a21
Nikita Tikhonyuk <nikitatikhonyuk@yandex.com>
2023-01-08 17:54:16 +1000

    Initial Commit

---
+++ RockPaperScissors/RockPaperScissorsApp.swift
@@ -0,0 +1,17 @@
+//
+//  RockPaperScissorsApp.swift
+//  RockPaperScissors
+//
+//  Created by Никита Тихонюк on 08.01.2023.
+//
+
+import SwiftUI
+
+@main
+struct RockPaperScissorsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
